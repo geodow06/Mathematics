6c95a8e28d439232561de9f9ca0f5334b29c04f3
George <georgedowne06@gmail.com>
2023-10-26 23:25:34 +0100

    Make numerical integration method inputs consistant

--- godmathlib/calculus/integration.py
+++ godmathlib/calculus/integration.py
@@ -4,13 +4,12 @@ import matplotlib.pyplot as plt
 __all__ = ['rungeKutta', 'euler', 'midpoint', 'pec']
 
 
-def rungeKutta(dydx, x0, y0, x, h):
+def rungeKutta(dydx, y0, x0, x, steps):
     # Count number of iterations using step size or
-    # step height h
-    n = (int)((x - x0) / h)
+    h = x / steps
     # Iterate for number of iterations
     y = y0
-    for i in range(1, n + 1):
+    for i in range(1, steps + 1):
         "Apply Runge Kutta Formulas to find next value of y"
         k1 = h * dydx(x0, y)
         k2 = h * dydx(x0 + 0.5 * h, y + 0.5 * k1)
@@ -25,42 +24,42 @@ def rungeKutta(dydx, x0, y0, x, h):
     return y
 
 
-def euler(dydt, y0, t0, steps, target_t):
-    """"Solves ODE by Euler method
+def euler(dydt, y0, t0, t, steps):
+    """Solves ODE by Euler method
 
     given a function dydt(y(t),t) and initial conditions y0 = y(t0)
     """
-    h = target_t / steps
-    t = [t0]
-    y = [y0]
+    h = t / steps
+    t_values = [t0]
+    y_values = [y0]
     for n in range(steps + 1):
         if n > 0:
-            new_t = t[0] + n * h
-            new_y = y[n - 1] + h * dydt(new_t, y[n - 1])
-            t.append(new_t)
-            y.append(new_y)
-    plt.plot(t, y, label="Euler")
-    target_y = y[-1]
-    return target_y
+            new_t = t_values[0] + n * h
+            new_y = y_values[n - 1] + h * dydt(new_t, y_values[n - 1])
+            t_values.append(new_t)
+            y_values.append(new_y)
+    plt.plot(t_values, y_values, label="Euler")
+    y = y_values[-1]
+    return y
 
 
-def midpoint(dydt, y0, t0, steps, target_t):
-    """"Solves ODE by Midpoint method
+def midpoint(dydt, y0, t0, t, steps):
+    """Solves ODE by Midpoint method
 
     given a function dydt(y(t),t) and initial conditions y0 = y(t0)
     """
-    t = [t0]
-    y = [y0]
-    h = target_t / steps
+    t_values = [t0]
+    y_values = [y0]
+    h = t / steps
     for n in range(steps + 1):
-        t_n = t[0] + n * h
+        t_n = t_values[0] + n * h
         half_step = h / 2
-        y_n_plus_one = y[n] + h * dydt(t_n + half_step, y[n] + half_step * dydt(t_n, y[n]))
-        t.append(t_n + h)
-        y.append(y_n_plus_one)
-    plt.plot(t, y, label="Midpoint")
-    target_y = y[-1]
-    return target_y
+        y_n_plus_one = y_values[n] + h * dydt(t_n + half_step, y_values[n] + half_step * dydt(t_n, y_values[n]))
+        t_values.append(t_n + h)
+        y_values.append(y_n_plus_one)
+    plt.plot(t_values, y_values, label="Midpoint")
+    y = y_values[-1]
+    return y
 
 
 def pec(f):
